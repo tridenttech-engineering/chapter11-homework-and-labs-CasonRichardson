03ca2882a7b18c42aab4523ba6cde8e75a2c8538
CasonRichardson <simpletheotter@gmail.com>
2025-04-19 16:25:26 -0400

    DONE

--- hw11-21.cpp
+++ hw11-21.cpp
@@ -6,27 +6,19 @@
 
 using namespace std;
 
-double getAverage(double prices[], int numElements);
-
 int main()
 {
 	double prices[10] = {96.5, 100.5, 100.5, 100.5, 99, 99, 99, 100, 98.5, 98.9};
 	double total = 0.0;
 	double average = 0.0;
 
-	average = getAverage(prices, 10);
-
+	for (int x = 0; x < 10, x += 1){
+		total += prices[x];
+		average = total / 10;
+}
 	cout << fixed << setprecision(1);
 	cout << "Average stock price: $" << average << endl;
 	return 0;
 }	//end of main function
 
-double getAverage(double prices[], int numElements)
-{
-	double total = 0.0;
-	for (int x = 0; x < numElements; x += 1) {
-		total += prices[x];
-}
-return total/numElements;
-}
-	
+
